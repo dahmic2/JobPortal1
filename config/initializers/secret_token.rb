021aa0b601b4c6b4c8abafe3fc5825524840ef93
dahmic2 <dahmic2@etf.unsa.ba>
2014-09-28 16:18:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobPortal::Application.config.secret_key_base = '9a315be5244812a35b12259943098749047620879a06bafe5005ca602b1df5a7bfc3630fe2beb19ec315ba1b4a9d71226be0e178b75f53aa218dce375258a0c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JobPortal::Application.config.session_store :cookie_store, key: '_JobPortal_session'
