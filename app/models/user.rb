94166c37208d55a2dc8a3127e818891a4d4f4649
dahmic2 <dahmic2@etf.unsa.ba>
2014-09-29 01:02:06 +0100

    home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 JobPortal::Application.routes.draw do
   resources :users
 
+  get "home/index"
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
